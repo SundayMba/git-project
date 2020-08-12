3c997de40d0257326dda985aa84ade76bfb68b78
sunday mba <sundinoh@gmail.com>
2020-08-12 21:30:35 +0100

    method to calculate fibonnaci

--- Program.cs
+++ Program.cs
@@ -8,9 +8,27 @@ namespace git_project
 {
     class Program
     {
+        /// <summary>
+        /// method to calculate fibonnci series
+        /// </summary>
+        /// <param name="arr"> stores the size of the arr</param>
+        static void fib(int[] arr)
+        {
+            
+            arr[0] = 1; arr[1] = 1;
+            int sum = 2;
+            for (int i = 2; i < arr.Length; i++)
+            {
+                arr[i] = arr[i - 1] + arr[i - 2];
+                sum += arr[i];
+            }
+            Console.WriteLine();
+        }
         static void Main(string[] args)
         {
-            Console.Write("hello  github");
+            int[] arr = new int[5];
+
+            Console.ReadLine();
         }
     }
 }
